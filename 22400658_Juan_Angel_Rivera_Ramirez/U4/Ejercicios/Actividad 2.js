7b4d1f278fc372be9e2217c59765c23ba66c237a
Angel <Juanriverara@ittepic.edu.mx>
2026-07-14 21:13:25 -0700

    Actividad 2 unidad 4 javascript

---
+++ 22400658_Juan_Angel_Rivera_Ramirez/U4/Ejercicios/Actividad 2.js
@@ -0,0 +1,118 @@
+/*
+1. Escribe un programa que genere dos números aleatorios y que escriba en
+consola qué números se generaron. Además, el programa deberá señalar si
+los números son iguales, y en caso de que no lo sean, mostrará cuál es el
+mayor.
+*/
+
+function Ejercicio_1(){
+    let n1 = Math.floor(Math.random() * 100) + 1;
+    let n2 = Math.floor(Math.random() * 100) + 1;
+
+    console.log("Numero 1 aleatorio generado: ", n1);
+    console.log("Numero 2 aleatorio generado: ", n2);
+
+    if (n1 === n2) {
+        console.log("Los números son iguales.");
+    } else if (n1 > n2) {
+        console.log("El numero mayor es:", n1);
+    } else {
+        console.log("El numero mayor es:", n2);
+    }
+}
+
+//Ejercicio_1();
+
+/*
+2. Escribe un programa que genere de forma continua números entre el 0 y el 5,
+hasta que salga el número 0.
+*/
+
+function Ejercicio_2(){
+    let Aleatorio;
+
+    do {
+        Aleatorio = Math.floor(Math.random() * 6);
+        console.log(Aleatorio);
+    } while (Aleatorio !== 0);
+}
+
+//Ejercicio_2();
+
+
+/*
+3. Elabora un programa que genere un número aleatorio entre el número 2 y el
+10, y después escriba la tabla de multiplicar del número generado.
+*/
+
+function Ejercicio_3(){
+    let Aleatorio = Math.floor(Math.random() * 9) + 2;
+    
+    console.log("Numero aleatorio generado: ", Aleatorio);
+
+    for (let i = 1; i <= 10; i++) {
+        console.log(Aleatorio + " * " + i + " = " + (Aleatorio * i));
+    }
+}
+
+//Ejercicio_3();
+
+/*
+4. Escribe un programa que solicite 10 números entre el 1 y el 100, 
+y determine cuántos de esos números son pares y cuántos impares.
+*/
+
+function Ejercicio_4(){
+    let par = 0;
+    let imp = 0;
+
+    for (let i = 1; i <= 10; i++) {
+
+        let num = Number(prompt("Iteracion: "+i+" - Ingresa un numero del 1 al 100: "));
+
+        if (num % 2 === 0) {
+            par++;
+        } else {
+            imp++;
+        }
+    }
+
+    console.log("Total de Pares: ", par);
+    console.log("Total de Impares: ", imp);
+}
+
+//Ejercicio_4();
+
+
+/*
+5. Una tienda departamental paga a sus empleados el 10% de comisión a
+quienes vendan menos de $10,000.00 en artículos, mientras que otorga un
+15% a quienes vendan $10,000.00 o más. 
+
+Diseña un programa que solicite un número entre $5,000 y $30,0000 
+(validar entrada hasta que sea número válido) 
+en ventas para un empleado, y muestre en pantalla cuánto dinero va a
+recibir por comisión.
+*/
+
+function Ejercicio_5(){
+    let ventas;
+
+    do {
+        ventas = Number(prompt(
+            "Ingresa las ventas del empleado ($5,000 - $30,000):"));
+    } while (isNaN(ventas) || ventas < 5000 || ventas > 30000);
+
+    let comision;
+
+    if (ventas < 10000) {
+        comision = ventas * 0.10;
+    } else {
+        comision = ventas * 0.15;
+    }
+
+    console.log("Ventas: $" + ventas);
+    console.log("Comisión: $" + comision);
+}
+
+Ejercicio_5();
---
+++ 22400658_Juan_Angel_Rivera_Ramirez/U4/Ejercicios/index.js
@@ -0,0 +1,131 @@
+var nombre = "JUAN ANGEL RIVERA RAMIREZ";
+let promedio = 9.5;
+const edad = 20;
+
+/*
+console.log(promedio);
+console.log(edad);
+console.log(nombre);
+
+if(true){
+    var libro = "Libro 1";
+    let animal = "perro";
+    animal = 5;
+    console.log(animal);
+}
+console.log(libro);
+*/
+
+/*
+let cadena = prompt("Ingresa una cadena");
+
+let n1 = parseInt(prompt("Ingresa un número"));
+let n2 = parseInt(prompt("Ingresa un número"));
+console.log(n1);
+console.log(n2);
+
+let suma = n1 + n2;
+console.log(suma);
+console.log(cadena);
+*/
+
+/*
+let suma = 10+5;
+let resta = 10-5;
+let multiplicacion = 10*5;
+let division = 10/5;
+let residuo = 10%5;
+
+console.log("Suma: ",suma);
+console.log("Resta: ",resta);
+console.log("Multiplicacion: ",multiplicacion);
+console.log("Division: ",division);
+console.log("Residuo: ",residuo);
+
+console.log(5>3);
+console.log(5<3);
+console.log(5>=3);
+console.log(5<=4);
+console.log(5 == "5");
+console.log(5 === "5");
+console.log(5 != "3");
+
+let calificacion = 90;
+if(calificacion >=90){
+    console.log("Excelente");
+} else if (calificacion >= 70){
+    console.log("Aprobado");
+} else {
+console.log("Reprobado");
+}
+
+let tabla = 5;
+for(let i=1; i<=10; i++){
+    console.log(`${tabla} x ${i} = ${tabla*i}`);
+    //console.log(tabla + " x "+i+" = "+tabla*i);
+}
+
+let contador = 1;
+
+while (contador<=5){
+    console.log(contador);
+    contador++;
+}
+
+function saludar(nombre){
+    console.log("Hola "+nombre+" eres gay");
+}
+
+saludar("Sebastian");
+
+function aleatorio(){
+    let n = Math.floor(Math.random()*1000+1);
+    console.log(n);
+}
+aleatorio();
+
+let numeros = [1,2,3];
+console.log(numeros);
+numeros.push(20);
+console.log(numeros);
+console.log(numeros[0]);
+
+let numeros = [5,2];
+numeros.push(20);
+numeros.push(21);
+numeros.push(22);
+numeros.push(23);
+numeros.push(24);
+console.log(numeros);
+
+for(let i = 0; i<numeros.length; i++){
+    console.log(i+" interacion = "+numeros[i]);
+}
+
+let persona = {
+    nombre: "Juan",
+    edad: 20,
+    ciudad: "Tepic"
+}
+console.log(persona.ciudad);
+console.log(persona.edad);
+console.log(persona.nombre);
+console.log(persona);
+
+persona.nombre = "Carlos"; // Asigna nuevamente
+persona.carrera = "ISC"; // Lo crea si no esta en el objeto persona
+console.log(persona.nombre);
+
+
+let numeros = [1,2,3,4,5];
+numeros.forEach(numero => {
+    console.log(numero);
+});
+
+let numeros = [1,2,3,4,5];
+let dobles =numeros.map(numero => {
+    return numero*2;
+});
+console.log(dobles);
+*/
+
