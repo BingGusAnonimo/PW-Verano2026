f1ad54683e92c48445e3ec80734fe9099a1bd575
Angel <Juanriverara@ittepic.edu.mx>
2026-07-23 16:57:55 -0700

    Api conexion

--- ServerBase/crud_peliculas/script.js
+++ ServerBase/crud_peliculas/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000";
+const API_URL = "https://api-proyect-sigma.vercel.app/peliculas";
 
 const formulario = document.getElementById("formulario");
 const id = document.getElementById("id");
