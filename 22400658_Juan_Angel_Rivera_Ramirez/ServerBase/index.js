cfb08269232a62fbbffe2ae159d656b56302263e
Angel <Juanriverara@ittepic.edu.mx>
2026-07-23 15:30:32 -0700

    script de index.js, pagina coneixion a arreglos

--- 22400658_Juan_Angel_Rivera_Ramirez/ServerBase/index.js
+++ 22400658_Juan_Angel_Rivera_Ramirez/ServerBase/index.js
@@ -1,7 +1,130 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
 const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+
 const PORT = 3000;
 
+let peliculas = [
+    {
+        id: 1,
+        titulo: "Interestelar",
+        director: "Cristopher Nolan",
+        año: 2014
+    },
+    {
+        id: 2,
+        titulo: "Titanic",
+        director: "Roberto",
+        año: 2010
+    }
+]
+
+let idActual = peliculas.length + 1;
+
+// Obtener todas las peliculas
+app.get("/peliculas",(req, res) => {
+    res.json(peliculas);
+});
+
+// Obtener una pelicula por ID
+app.get("/peliculas/:id",(req, res) => {
+    const id = Number(req.params.id);
+    const pelicula = peliculas.find(
+        pelicula => pelicula.id == id
+    );
+    if(!pelicula){
+        return res.status(404).json({
+            mensaje: "Pelicula no encontrada"
+        });
+    }
+    res.json(pelicula);
+});
+
+// Registrar pelicula
+app.post("/peliculas",(req, res) => {
+    const { titulo, director, año} = req.body;
+    if(!titulo || !director || !año){
+        return res.status(400).json({
+            mensaje: "Faltan datos de la pelicula"
+        });
+    };
+
+    const nuevaPelicula = {
+        id: idActual++,
+        titulo: titulo,
+        director: director,
+        año: Number(año)
+    };
+
+    peliculas.push(nuevaPelicula);
+    res.status(201).json({
+        mensaje: "Pelicula registrada con exito",
+        pelicula: nuevaPelicula
+    });
+});
+
+// Actualizar una pelicula
+app.put("/peliculas/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const {titulo, director, año} = req.body;
+
+    if(!titulo || !director || !año) {
+        return res.status(400).json({
+            mensaje: "Faltan datos de la pelicula"
+        });
+    }
+    const indice = peliculas.findIndex(
+        pelicula => pelicula.id === id
+    );
+
+    if(indice === -1){
+        return res.status(400).json({
+            mensaje: "Pelicula no encontrada"
+        });
+    }
+
+    peliculas[indice] = {
+        id: id,
+        titulo: titulo,
+        director: director,
+        año: Number(año)
+    }
+
+    res.json({
+        mensaje: "Pelicula actualizado correctamente",
+        pelicula: peliculas[indice]
+    });
+});
+
+app.delete("/peliculas/:id", (req, res) =>{
+    const id = Number(req.params.id);
+    const indice = peliculas.findIndex(
+        pelicula => pelicula.id === id
+    );
+
+    if(indice === -1){
+        return res.status(404).json({
+            mensaje: "Pelicula no encontrada"
+        })
+    }
+
+    const peliculaEliminada = peliculas[indice];
+
+    peliculas.splice(indice, 1);
+
+    res.json({
+        mensaje: "Pelicula eliminada correctamente",
+        pelicula: peliculaEliminada
+    });
+});
+
+
+// Ejercicios de inicio ---------------------------------------
 app.get("/", (req, res) => {
     res.send("Bienvenido a mi primer servidor con express");
 });
